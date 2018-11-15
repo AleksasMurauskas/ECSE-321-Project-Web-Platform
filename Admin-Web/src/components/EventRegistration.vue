1aaeeeda008ccd5ac26b833244a0762709e83357
Jacob McConnell <jacob.mcconnell@mail.mcgill.ca>
2018-11-15 12:22:03 -0500

    doing tutorial example

---
+++ Admin-Web/src/components/EventRegistration.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="eventregistration">
+    <h2>Participants</h2>
+    <table>
+      <tr v-for="participant in participants" >
+      	<td>{{ participant.name }}</td>
+      	<td>
+        	<ul>
+          		<li v-for="event in participant.events">
+           		 {{event.name}}
+          		</li>
+        	</ul>
+      	</td>
+  	  </tr>
+
+      <tr>
+    <td>
+        <input type="text" v-model="newParticipant" placeholder="Participant Name">
+    </td>
+    <td>
+        <button @click="createParticipant(newParticipant)">Create Participant</button>
+    </td>
+  </tr>
+    </table>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+
+
+
+<script src="./registration.js">
+</script>
+
+
+
+</template>
+<style>
+  #eventregistration {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
+<span v-if="errorParticipant" style="color:red">Error: {{errorParticipant}} </span>
+
+
--- Admin-Web/src/router/index.js
+++ Admin-Web/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Ranking from '@/components/Ranking'
+import EventRegistration from '@/components/EventRegistration'
 
 Vue.use(Router)
 
@@ -15,7 +16,12 @@ export default new Router({
      {
     path: '/Ranking',
     name: 'Ranking',
-    component: Ranking 
+    component: Ranking
+    },
+    {
+      path: '/app',
+      name: 'EventRegistration',
+      component: EventRegistration
     }
   ]
 })
