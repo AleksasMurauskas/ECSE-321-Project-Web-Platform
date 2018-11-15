9760343781071939bed56072f0d8021c6c55968f
Karl Godin <godinkarl@gmail.com>
2018-11-15 01:37:57 -0500

    Basic view for ranking

---
+++ Admin-Web/src/components/item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="item">{{index + 1}} {{ text }}</div>
+</template>
+
+<script>
+    export default {
+        props: {
+            index: Number,
+            text: "temp"
+        }
+    }
+</script>
+
+<style>
+    .item {
+        height: 50px;
+        line-height: 50px;
+        padding-left: 20px;
+        border-bottom: 1px solid #eee;
+    }
+</style>
