8c5653c1d046c34d8ce17491bd18cf50a05ca395
Jacob McConnell <jacob.mcconnell@mail.mcgill.ca>
2018-11-16 02:08:19 -0500

    very rough outline of trips MVP no search yet

--- Admin-Web/src/components/EventRegistration.vue
+++ Admin-Web/src/components/EventRegistration.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="eventregistration">
-    <h2>Participants</h2>
+    <h2>OverView</h2>
     <table>
-      <tr v-for="participant in participants" >
-      	<td>{{ participant.name }}</td>
+      <tr v-for="trip in trips" >
+      	<td>{{ trip.iD }}</td>
+        <td>{{ trip.startPoint }}</td>
+        <td>{{ trip.endPoint }}</td>
+        <td>
+          	<button @click="selectTrip(trip)"> select</button>
+      	</td>
+
       	<td>
         	<ul>
-          		<li v-for="event in participant.events">
-           		 {{event.name}}
+          		<li v-for="customer in trip.customers">
+           		 {{customer.name}}
           		</li>
         	</ul>
       	</td>
+
+
   	  </tr>
 
     	<tr>
@@ -22,6 +30,61 @@
     	</td>
   </tr>
     </table>
+
+
+
+
+
+    <h2> Trip Info </h2>
+    <table>
+        <tr>
+          <th>Trip Times</th>
+          <th>Trip Locations</th>
+          <th>Routes</th>
+        </tr>
+        <tr>
+          <th class ="anim:id">TripID</th>
+          <th class ="anim:id">Driver</th>
+          <th class ="anim:starttime">StartTime</th>
+          <th class ="anim:endtime">EndTime</th>
+          <th class ="anim:startpoint">StartPoint</th>
+          <th class ="anim:endpoint">EndPoint</th>
+          <th class ="anim:tripnodes">TripNodes</th>
+          <th class ="anim:tripnodes">Customers</th>
+        </tr>
+      <tbody>
+        <tr>
+            <td>{{ sELECTEDTRIP.iD  }}</td>
+            <td>{{ sELECTEDTRIP.driver  }}</td>
+            <td> {{ sELECTEDTRIP.startTime  }}  </td>
+            <td>  {{ sELECTEDTRIP.endTime  }} </td>
+            <td> {{ sELECTEDTRIP.startPoint  }} </td>
+            <td> {{ sELECTEDTRIP.endPoint  }} </td>
+            <td> <tr v-for="tripnode in sELECTEDTRIP.tripNodes ">
+
+              <td> {{tripnode.name}} </td>
+            </tr>
+            </td>
+
+            <td> <tr v-for="customer in sELECTEDTRIP.customers ">
+
+              <td> {{customer.name}} </td>
+            </tr>
+            </td>
+
+        </tr>
+
+
+      </tbody>
+    </table>
+
+
+
+
+
+
+
+
     <p>
       <span style="color:red">Error: Message text comes here</span>
     </p>
@@ -45,5 +108,3 @@
   }
 </style>
 <span v-if="errorParticipant" style="color:red">Error: {{errorParticipant}} </span>
-
-
--- Admin-Web/src/components/registration.js
+++ Admin-Web/src/components/registration.js
@@ -1,33 +1,44 @@
-function ParticipantDto (name) {
+function CustomerDto (name) {
   this.name = name
   this.events = []
 }
 
-function EventDto (name, date, start, end) {
+function TripNodeDto (name) {
   this.name = name
-  this.eventDate = date
+  this.events = []
+}
+
+function TripDto (iD, driver, start, end, startPoint, endPoint) {
+  this.iD = iD
+  this.startPoint = startPoint
+  this.endPoint= endPoint
+  this.driver = driver
   this.startTime = start
   this.endTime = end
+  this.customers=[]
+  this.tripNodes=[]
 }
 
 export default {
   name: 'eventregistration',
   data () {
     return {
-      participants: [],
+      trips: [],
       newParticipant: '',
       errorParticipant: '',
+      sELECTEDTRIP : new TripDto("000", "000", "0000", "0000","000000","000000"),
       response: []
     }
   },
   created: function () {
   // Test participants
-  const p1 = new ParticipantDto('John')
-  const p2 = new ParticipantDto('Jill')
+  const p1 = new TripDto("1", "bob", "17:30", "18:00","montreal","quebec")
+  const p2 = new TripDto("1", "bob", "17:30", "18:00","toronto","montreal")
   // Test event for p2 participant
-  p2.events=[{"name":"Halloween party","eventDate":"2018-10-31","startTime":"18:00","endTime":"23:00"}]
+  p2.customers=[{"name":"ricky"}]
+  p2.tripNodes = [{"name":"kingston"}]
   // Sample initial content
-  this.participants = [p1, p2]
+  this.trips = [p1, p2]
 },
 
 
@@ -38,8 +49,18 @@ methods: {
     this.participants.push(p)
     // Reset the name field for new participants
     this.newParticipant = ''
+  },
+
+  selectTrip: function (trip) {
+    // Create a new participant and add it to the list of participants
+    //var p = new ParticipantDto(participantName)
+    //this.p.push(p)
+    // Reset the name field for new participants
+    //this.newParticipant = ''
+    this.sELECTEDTRIP = trip
   }
-}
+
 
 }
 
+}
